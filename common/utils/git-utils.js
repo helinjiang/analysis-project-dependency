e99dbc9a98cb8f47507863bbce5678369bcd6291
helinjiang <me@helinjiang.com>
2017-01-13 17:28:11 +0800

    增加对package.json路径的支持，因为某些项目里面，package.json不一定在根目录下

--- common/utils/git-utils.js
+++ common/utils/git-utils.js
@@ -1,4 +1,5 @@
-var Git = require("nodegit");
+var path = require('path');
+var Git = require('nodegit');
 
 /**
  * Clone一份git代码到本地指定的目录下
@@ -17,19 +18,30 @@ function open(localPath) {
 /**
  * 获得某个本地仓库中package.json的文件对象
  * @param {String} localPath 本地路径
+ * @param {String} [packageFileRelativePath] package.json 在仓库中的相对路径
  * @return {Promise}
  */
-function getPackageJson(localPath) {
+function getPackageJson(localPath, packageFileRelativePath) {
     return open(localPath)
         .then(function (repo) {
             return repo.getMasterCommit();
         })
         .then(function (commit) {
-            return commit.getEntry("package.json");
+            return commit.getTree();
         })
-        .then(function (entry) {
+        .then(function (tree) {
+            if (!packageFileRelativePath) {
+                packageFileRelativePath = './';
+            }
+
+            // 这里注意在window下path.join生成的路径为 a\\b\\c.js，但这里需要的是a/b/c.js，因此要处理一下
+            var filePath = path.join(packageFileRelativePath, 'package.json').replace(/\\/ig, '/');
+
+            return tree.getEntry(filePath);
+        })
+        .then(function (treeEntry) {
             // Get the blob contents from the file.
-            return entry.getBlob();
+            return treeEntry.getBlob();
         })
         .then(function (blob) {
             return JSON.parse(String(blob));
